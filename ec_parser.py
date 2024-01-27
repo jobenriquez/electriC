e4544d9fa85fb796784c05e0c0002e24f2bb06dd
Job <jobiskits@gmail.com>
2024-01-28 04:08:07 +0800

    Implemented input, output, ec statements

--- ec_parser.py
+++ ec_parser.py
@@ -74,6 +74,39 @@ class UnaryStatementNode:
     def __repr__(self):
         return f'AssignmentStatementNode({self.identifier}, {self.unary_op})'
     
+class InputStatementNode:
+    def __init__(self, identifier, scan_statement):
+        self.identifier = identifier
+        self.scan_statement = scan_statement
+    
+    def __repr__(self):
+        return f'InputStatementNode({self.identifier}, {self.scan_statement})'
+    
+class OutputStatementNode:
+    def __init__(self, output_statement, initialization):
+        self.output_statement = output_statement
+        self.initialization = initialization
+    
+    def __repr__(self):
+        return f'OutputStatementNode({self.output_statement}, {self.initialization})'
+    
+class ECKeywordNode:
+    def __init__(self, var1, op, var2):
+        self.var1 = var1
+        self.op = op
+        self.var2 = var2
+    
+    def __repr__(self):
+        return f'ECKeywordNode({self.var1}, {self.op}, {self.var2})'
+    
+class SquareRootNode:
+    def __init__(self, node):
+        self.node = node
+
+    def __repr__(self):
+        return f'SquareRootNode({self.node})'
+
+    
 #############################
 #           Parser          #
 #############################
@@ -92,6 +125,10 @@ class Parser:
     def peek_next_token(self):
         if self.token_index < len(self.tokens):
             return self.tokens[self.token_index]
+    
+    def peek_token_after_next(self):
+        if self.token_index < len(self.tokens):
+            return self.tokens[self.token_index+1]
 
     def match_type(self, expected_type):
         if self.current_token.type_ == expected_type:
@@ -133,7 +170,7 @@ class Parser:
             current_statement = self.identify_statements()
             statements.append(current_statement)
             if self.current_token.type_ == 'DEL_SEMICOLON':
-                self.consume_token()  # Consume the semicolon
+                self.consume_token() 
             else:
                 raise SyntaxError("Expected ';' after statement")
 
@@ -144,6 +181,10 @@ class Parser:
             return self.parse_declaration_statement()
         if self.current_token.type_ == 'IDENTIFIER':
             return self.parse_assignment_unary_statement()
+        if self.current_token.type_ == 'RESERVED_WORD' and self.current_token.value in ['Print', 'PrintLine']:
+            return self.parse_output_statement()
+        else:
+            raise SyntaxError("Invalid statement encountered")
         
     def parse_declaration_statement(self):
         if self.token_index < len(self.tokens):
@@ -160,7 +201,7 @@ class Parser:
             self.consume_token() 
             return data_type
         else:
-            raise SyntaxError("Invalid data type")
+            raise SyntaxError("Invalid data type encountered")
 
     def parse_variable_list(self, expected_data_type):
         identifier_list = []
@@ -178,7 +219,6 @@ class Parser:
                 self.match_type('DEL_COMMA')
                 next_identifier = self.parse_identifier()
                 next_initialization = self.parse_initialization(expected_data_type)
-
                 identifier_list.append((next_identifier, next_initialization))
         else:
             raise SyntaxError(f"Expected an identifier, but found {self.current_token.type_}")
@@ -193,51 +233,36 @@ class Parser:
             raise SyntaxError(f"Expected an identifier, but found {self.current_token.type_}")
     
     def parse_initialization(self, expected_data_type):
-        if expected_data_type == "string" or "char":
+        if expected_data_type in ['int', 'float', 'integer', None]:
             if self.current_token.type_ in TOKEN_ASS_OPS:
-                type_of_delimeter = None
-                #self.match_type('OP_ASS')
                 self.consume_token()
-
-                if self.current_token.type_ == 'DEL_DBLQUOTE': 
-                    self.match_type('DEL_DBLQUOTE')
-                    type_of_delimeter = 'DEL_DBLQUOTE' # Save the type of delimiter used for checking
-                elif self.current_token.type_ == 'DEL_SGLQUOTE':
-                    self.match_type('DEL_SGLQUOTE')
-                    type_of_delimeter = 'DEL_SGLQUOTE'
-
-                try:
-                    initialization = self.parse_literal(expected_data_type)
-                    if type_of_delimeter == 'DEL_DBLQUOTE': 
-                        self.match_type('DEL_DBLQUOTE') # Ensure that the delimiter used matches
-                    elif type_of_delimeter == 'DEL_SGLQUOTE':
-                        self.match_type('DEL_SGLQUOTE')
-                    return initialization
-                except SyntaxError as e:
-                    raise SyntaxError(f"Error in variable initialization: {e}")
+                initialization = self.parse_literal(expected_data_type)
+                return initialization
             else:
                 return None
-        elif expected_data_type == "int" or "float":
+        elif expected_data_type in ['string', 'char', 'bool', 'boolean', None]:
+            if self.current_token.type_ not in ['OP_ASS', None]:
+                raise SyntaxError(f"Expected an assignment operator, but found {self.current_token.type_}")
             if self.current_token.type_ == 'OP_ASS':
-                self.match_type('OP_ASS')
-                try:
-                    initialization = self.parse_literal(expected_data_type)
-                    return initialization
-                except SyntaxError as e:
-                    raise SyntaxError(f"Error in variable initialization: {e}")
+                self.consume_token()
+                initialization = self.parse_literal(expected_data_type)
+                return initialization
             else:
                 return None
+            
+        else:
+            raise SyntaxError(f"Unexpected token: {self.current_token.type_}")
 
     def parse_literal(self, expected_data_type):
         if self.current_token.type_ == 'IDENTIFIER':
             return self.parse_identifier()
         elif self.current_token.type_ == 'DEL_LPAREN':
             return self.parse_additive_expression()
-        elif self.current_token.type_ == 'LIT_INT' and expected_data_type == 'int':
+        elif self.current_token.type_ == 'LIT_INT' and expected_data_type == 'int' or expected_data_type == 'integer':
             return self.parse_additive_expression()
         elif self.current_token.type_ == 'LIT_FLT' and expected_data_type == 'float':
             return self.parse_additive_expression()
-        elif self.current_token.type_ == 'LIT_CHAR' and expected_data_type == 'char':
+        elif self.current_token.type_ == 'LIT_CHAR' and expected_data_type == 'char' or expected_data_type == 'character':
             literal = self.current_token.value
             self.consume_token()
             return LiteralNode(literal)
@@ -253,6 +278,8 @@ class Parser:
             literal = self.current_token.value
             self.consume_token()
             return LiteralNode(literal)
+        elif self.current_token.value == 'Scan':
+            return self.parse_input_statement()
         else:
             raise SyntaxError(f"Expected literal of type {expected_data_type}, but found {self.current_token.type_}")
 
@@ -302,20 +329,73 @@ class Parser:
         else:
             raise SyntaxError(f"Unexpected token: {self.current_token.type_}")
         
-    def get_data_type_value(self, literal_type):
-        return TOKEN_DATA_TYPE_VALUES[literal_type]
+    def parse_input_statement(self):
+        self.consume_token()
+        if self.current_token.type_ == 'DEL_LPAREN':
+            self.consume_token()
+            if self.current_token.type_ == 'DEL_RPAREN':
+                return 'Scan()'
+            else:
+                raise SyntaxError(f"Expected ')', but found {self.current_token.type_}")
+        else:
+            raise SyntaxError(f"Expected '(', but found {self.current_token.type_}")
+        
+    def parse_output_statement(self):
+        output_statement = self.current_token.value
+        self.consume_token()
+        if self.current_token.type_ == 'DEL_LPAREN':
+            self.consume_token()
+            if self.current_token.type_ == 'LIT_INT':
+                value = self.parse_literal('int')
+            elif self.current_token.type_ == 'KEYWORD':
+                value = self.parse_ec_keywords()
+            elif self.current_token.type_ == 'LIT_FLT':
+                value = self.parse_literal('float')
+            elif self.current_token.type_ == 'LIT_STR':
+                value = self.parse_literal('string')
+            elif self.current_token.type_ == 'LIT_CHAR':
+                value = self.parse_literal('char')
+            elif self.current_token.type_ == 'LIT_BOOLTRUE':
+                value = self.parse_literal('bool')
+            elif self.current_token.type_ == 'LIT_BOOLFALSE':
+                value = self.parse_literal('bool')
+            elif self.current_token.type_ == 'IDENTIFIER':
+                value = self.parse_identifier()
+            if self.current_token.type_ == 'DEL_RPAREN':
+                self.consume_token()
+                return OutputStatementNode(f'{output_statement}()', value)
+            else:
+                raise SyntaxError(f"Expected ')', but found {self.current_token.type_}")
+        else:
+            raise SyntaxError(f"Expected '(', but found {self.current_token.type_}")
         
     def parse_assignment_unary_statement(self):
         if self.current_token.type_ == 'IDENTIFIER':
             identifier = self.parse_identifier()
             next_token = self.peek_next_token()
-
-            if self.current_token.type_ in TOKEN_ASS_OPS:
-                if next_token.type_ in TOKEN_DATA_TYPE_VALUES:
-                    ass_op_type = self.current_token.value
-                    data_type = self.get_data_type_value(next_token.type_)
-                    value = self.parse_initialization(data_type)
-                    return AssignmentStatementNode(identifier, ass_op_type, value)
+            if next_token.value in KEYWORDS:
+                self.consume_token()
+                return self.parse_ec_keywords()
+            elif self.current_token.type_ == 'OP_ASS' and next_token.value == 'Scan':
+                self.consume_token()
+                scan = self.parse_input_statement()
+                self.consume_token()
+                return InputStatementNode(identifier, scan)
+            elif self.current_token.type_ in TOKEN_ASS_OPS and next_token.type_ not in ['LIT_STR', 'LIT_CHAR']:
+                ass_op_type = self.current_token.value
+                self.consume_token()
+                value = self.parse_additive_expression()
+                return AssignmentStatementNode(identifier, ass_op_type, value)
+            elif self.current_token.type_ == 'OP_ASS' and next_token.type_ == 'LIT_STR':
+                ass_op_type = self.current_token.value
+                self.consume_token()
+                value = self.parse_literal('string')
+                return AssignmentStatementNode(identifier, ass_op_type, value)
+            elif self.current_token.type_ == 'OP_ASS' and next_token.type_ == 'LIT_CHAR':
+                ass_op_type = self.current_token.value
+                self.consume_token()
+                value = self.parse_literal('char')
+                return AssignmentStatementNode(identifier, ass_op_type, value)
             elif self.current_token.type_ in TOKEN_UNR_OPS:
                 unary_op = self.current_token.value
                 self.consume_token()
@@ -324,3 +404,127 @@ class Parser:
                 raise SyntaxError(f"Expected an assignment or unary operator, but found {self.current_token.type_}")
         else:
             raise SyntaxError(f"Expected an identifier, but found {self.current_token.type_}")
+        
+    def parse_ec_keywords(self):
+        if self.current_token.value == 'CV1':
+            return self.parse_square_root(self.two_param_keyword('OP_MUL: *'))
+        elif self.current_token.value == 'CV2':
+            return self.two_param_keyword('OP_DIV: /')
+        elif self.current_token.value == 'CV3':
+            return self.two_param_keyword('OP_MUL: *')
+        elif self.current_token.value == 'CVK':
+            return self.one_param_keyword('OP_DIV: /', 1000)
+        elif self.current_token.value == 'CKV':
+            return self.one_param_keyword('OP_MUL: *', 1000)
+        elif self.current_token.value == 'CVM':
+            return self.one_param_keyword('OP_MUL: *', 1000)
+        elif self.current_token.value == 'CMV':
+            return self.one_param_keyword('OP_DIV: /', 1000)
+        elif self.current_token.value == 'CMK':
+            return self.one_param_keyword('OP_DIV: /', 1000000)
+        elif self.current_token.value == 'CO1':
+            return self.two_param_keyword('OP_DIV: /')
+        elif self.current_token.value == 'CO2':
+            return self.two_param_keyword('OP_DIV: /')
+        elif self.current_token.value == 'CO3':
+            return self.two_param_keyword('OP_DIV: /')
+        elif self.current_token.value == 'CW1':
+            return self.two_param_keyword('OP_DIV: /')
+        elif self.current_token.value == 'CW2':
+            return self.two_param_keyword('OP_MUL: *')
+        elif self.current_token.value == 'CW3':
+            return self.two_param_keyword('OP_MUL: *')
+        elif self.current_token.value == 'CWK':
+            return self.one_param_keyword('OP_DIV: /', 1000)
+        elif self.current_token.value == 'CKM':
+            return self.one_param_keyword('OP_MUL: *', 1000)
+        elif self.current_token.value == 'CMW':
+            return self.one_param_keyword('OP_MUL: *', 1000000)
+        elif self.current_token.value == 'CWM':
+            return self.one_param_keyword('OP_DIV: /', 1000000)
+        elif self.current_token.value == 'CWH':
+            return self.one_param_keyword('OP_DIV: /', 745.7)
+        elif self.current_token.value == 'CKH':
+            return self.one_param_keyword('OP_MUL: *', 1.341)
+        elif self.current_token.value == 'CMH':
+            return self.one_param_keyword('OP_MUL: *', 1341)
+        elif self.current_token.value == 'CHW':
+            return self.one_param_keyword('OP_MUL: *', 745.7)
+        elif self.current_token.value == 'CHK':
+            return self.one_param_keyword('OP_DIV: /', 1.341)
+        elif self.current_token.value == 'CHM':
+            return self.one_param_keyword('OP_DIV: /', 1341)
+        elif self.current_token.value == 'CA1':
+            return self.two_param_keyword('OP_DIV: /')
+        elif self.current_token.value == 'CA2':
+            return self.two_param_keyword('OP_DIV: /')
+        elif self.current_token.value == 'CA3':
+            return self.parse_square_root(self.two_param_keyword('OP_DIV: /'))
+        elif self.current_token.value == 'CMA':
+            return self.one_param_keyword('OP_DIV: /', 1000)
+        elif self.current_token.value == 'CAM':
+            return self.one_param_keyword('OP_MUL: *', 1000)
+        elif self.current_token.value == 'CCH':
+            return self.two_param_keyword('OP_MUL: *')
+        elif self.current_token.value == 'CCA':
+            return self.two_param_keyword('OP_DIV: /')
+        elif self.current_token.value == 'CHTM':
+            return self.one_param_keyword('OP_MUL: *', 60)
+        elif self.current_token.value == 'CMTS':
+            return self.one_param_keyword('OP_MUL: *', 60)
+        elif self.current_token.value == 'CHTS':
+            return self.one_param_keyword('OP_MUL: *', 3600)
+        elif self.current_token.value == 'CMTH':
+            return self.one_param_keyword('OP_DIV: /', 60)
+        elif self.current_token.value == 'CSTM':
+            return self.one_param_keyword('OP_DIV: /', 60)
+        elif self.current_token.value == 'CSTH':
+            return self.one_param_keyword('OP_DIV: /', 3600)
+        else:
+            raise SyntaxError("Invalid reserved word encountered")
+
+    def two_param_keyword(self, op):
+        self.consume_token()
+        if self.current_token.type_ == 'DEL_LPAREN':
+            self.consume_token()
+            if self.current_token.type_ in ['LIT_INT', 'LIT_FLT']:
+                var1 = self.current_token.value
+                self.consume_token()
+                if self.current_token.type_ == 'DEL_COMMA':
+                    self.consume_token()
+                    if self.current_token.type_ in ['LIT_INT', 'LIT_FLT']:
+                        var2 = self.current_token.value
+                        self.consume_token()
+                        if self.current_token.type_ == 'DEL_RPAREN':
+                            self.consume_token()
+                            return ECKeywordNode(var1, op, var2)
+                        else:
+                            raise SyntaxError(f"Expected ')', but found {self.current_token.type_}")
+                    else:
+                        raise SyntaxError(f"Expected literal of type int/float, but found {self.current_token.type_}")
+                else:
+                    raise SyntaxError(f"Expected a comma, but found {self.current_token.type_}")
+            else:
+                raise SyntaxError(f"Expected literal of type int/float, but found {self.current_token.type_}")
+        else:
+            raise SyntaxError(f"Expected '(', but found {self.current_token.type_}")
+        
+    def one_param_keyword(self, op, var2):
+        self.consume_token()
+        if self.current_token.type_ == 'DEL_LPAREN':
+            self.consume_token()
+            if self.current_token.type_ in ['LIT_INT', 'LIT_FLT']:
+                var1 = self.current_token.value
+                self.consume_token()
+                if self.current_token.type_ == 'DEL_RPAREN':
+                    self.consume_token()
+                    return ECKeywordNode(var1, op, var2)
+                else:
+                    raise SyntaxError(f"Expected ')', but found {self.current_token.type_}")
+            else:
+                raise SyntaxError(f"Expected literal of type int/float, but found {self.current_token.type_}")
+        else:
+            raise SyntaxError(f"Expected '(', but found {self.current_token.type_}")
+        
+    def parse_square_root(self, node):
+        return SquareRootNode(node)
